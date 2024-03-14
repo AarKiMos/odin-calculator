b0a74adfac88ddacc9170664a9087bdbf2a4b4c9
Aachman Mittal <m.aachman@gmail.com>
2024-03-15 00:59:41 +0530

    Add functions to update display

--- script.js
+++ script.js
@@ -29,3 +29,45 @@ function operate(opA, opB, operator) {
   }
 }
 
+function updateDisplay(value) {
+  screen = document.getElementById("value-display");
+  if (typeof value !== "number") {
+    screen.innerText = "dISp. Error";
+    return "ERROR";
+  }
+
+  let valueString;
+
+  let digitString = value.toString().replace(/\./g, "");
+  if (digitString.length > 10) {
+    valueString = value.toExponential(2);
+  } else {
+    valueString = value.toString();
+  }
+  console.log(valueString);
+  screen.innerText = valueString;
+}
+
+updateDisplay(0);
+
+function updateSignDisplay(sign) {
+  const signDisplay = document.getElementById("sign-display");
+  switch (sign) {
+    case "+":
+      signDisplay.innerText = "+";
+      break;
+    case "-":
+      signDisplay.innerText = "\u2212";
+      break;
+    case "*":
+      signDisplay.innerText = "\u00d7";
+      break;
+    case "/":
+      signDisplay.innerText = "\u00f7";
+      break;
+    default:
+      signDisplay.innerText = "";
+  }
+}
+
+updateSignDisplay("");
